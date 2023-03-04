90b0b89c9efa6648ad2bfb14db1761e8ef6d9101
StarJune <partis98@kakao.com>
2023-03-04 22:00:44 +0900

    DTO 상속 구성으로 변경

--- src/auth/model/auth.entity.ts
+++ src/auth/model/auth.entity.ts
@@ -1,35 +1,55 @@
 import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNumber, IsString } from 'class-validator';
 
 @Entity()
 export class AuthEntity {
   @PrimaryColumn()
+  @ApiProperty({ description: '유저 고유번호' })
   _id: string;
 
+  @ApiProperty({ description: '일반 사용자일 경우 True', required: true })
   @Column()
+  @IsBoolean()
   common: boolean;
 
+  @ApiProperty({ description: '아이디', required: true })
   @Column()
   @Index({ unique: true })
+  @IsString()
   id: string;
 
+  @ApiProperty({ description: '비밀번호', required: true })
   @Column()
+  @IsString()
   pw: string;
 
+  @ApiProperty({ description: '사용자 이름', required: true })
   @Column()
+  @IsString()
   name: string;
 
+  @ApiProperty({ description: '국가 정보', required: true })
   @Column()
+  @IsString()
   nation: string;
 
+  @ApiProperty({ description: '이메일', required: true })
   @Column()
+  @IsEmail()
   email: string;
 
+  @ApiProperty({ description: '휴대폰 번호 (국가 코드 불필요)', required: true })
   @Column()
+  @IsNumber()
   phone: number;
 
+  @ApiProperty({ description: '사용할 닉네임', required: true })
   @Column()
+  @IsString()
   nickname: string;
 
+  @ApiProperty({ description: '프로필 이미지 저장 경로' })
   @Column()
   profileimg: string;
 }
--- src/auth/model/dto/auth.join.dto.ts
+++ src/auth/model/dto/auth.join.dto.ts
@@ -1,36 +1,4 @@
-import { IsBoolean, IsEmail, IsNumber, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { OmitType, PickType } from '@nestjs/swagger';
+import { AuthEntity } from '../auth.entity';
 
-export class AuthJoinDto {
-  @ApiProperty({ description: '일반 사용자일 경우 True', required: true })
-  @IsBoolean()
-  common: boolean;
-
-  @ApiProperty({ description: '아이디', required: true })
-  @IsString()
-  id: string;
-
-  @ApiProperty({ description: '비밀번호', required: true })
-  @IsString()
-  pw: string;
-
-  @ApiProperty({ description: '사용자 이름', required: true })
-  @IsString()
-  name: string;
-
-  @ApiProperty({ description: '국가 정보', required: true })
-  @IsString()
-  nation: string;
-
-  @ApiProperty({ description: '이메일', required: true })
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({ description: '휴대폰 번호 (국가 코드 불필요)', required: true })
-  @IsNumber()
-  phone: number;
-
-  @ApiProperty({ description: '사용할 닉네임', required: true })
-  @IsString()
-  nickname: string;
-}
+export class AuthJoinDto extends OmitType(AuthEntity, ['_id', 'profileimg']) {}
