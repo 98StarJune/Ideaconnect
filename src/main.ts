48f73e540d2bf642064fb2c7fd5b0ace82863526
StarJune <partis98@kakao.com>
2023-03-04 22:01:14 +0900

    CORS 임시 허용 처리

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   const config = new DocumentBuilder()
     .setTitle('Idea Connect API Document')
     .setDescription(
-      '2023 Google Solution Challenge Project : Created by 배성준',
+      '2023 Google Solution Challenge Project : Created by 배성준'
     )
     .setVersion('0.0.1')
     .addTag('ideaconnect')
