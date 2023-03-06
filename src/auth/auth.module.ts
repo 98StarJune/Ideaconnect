f101ee40b1e0066384bcc4ac2c9d4a0f35244ea7
StarJune <partis98@kakao.com>
2023-03-07 08:54:38 +0900

    auth models moved

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,9 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './model/User.entity';
-import { NormalResponseDto } from './model/dto/response/normal.response.dto';
-import { ErrorResponseDto } from './model/dto/response/error.response.dto';
+import { UserEntity } from '../model/User.entity';
+import { NormalResponseDto } from '../model/dto/response/normal.response.dto';
+import { ErrorResponseDto } from '../model/dto/response/error.response.dto';
 import { JwtService } from '@nestjs/jwt';
 import { JwtMiddleware } from '../jwt/jwt.middleware';
 
