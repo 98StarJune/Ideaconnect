ca09a417405ff67cb9e52a81e19dcd864a80c0f5
StarJune <partis98@kakao.com>
2023-03-08 16:13:31 +0900

    Response Entity 설명 수정

--- src/model/response.entity.ts
+++ src/model/response.entity.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseEntity {
-  @ApiProperty({ description: '에러 코드' })
+  @ApiProperty({ description: '상태코드' })
   statusCode: number;
-  @ApiProperty({ description: '에러 메세지' })
+
+  @ApiProperty({ description: '메세지' })
   message: string;
-  @ApiProperty({ description: '에러 객체' })
+
+  @ApiProperty({ description: '에러 메세지' })
   error: object;
 }
