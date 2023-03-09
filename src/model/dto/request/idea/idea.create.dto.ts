c98920f9ca19e37b78b31a334a6e85664ab4918b
StarJune <partis98@kakao.com>
2023-03-09 20:00:14 +0900

    Create 시 게시글 고유번호 validation 해결

--- src/model/dto/request/idea/idea.create.dto.ts
+++ src/model/dto/request/idea/idea.create.dto.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { IdeaEntity } from '../../../idea.entity';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
 
 @Injectable()
-export class IdeaCreateDto extends IdeaEntity {}
+export class IdeaCreateDto extends OmitType(IdeaEntity, ['_id']) {
+  @ApiProperty({ description: '게시글 고유 번호', deprecated: true })
+  _id: number;
+}
