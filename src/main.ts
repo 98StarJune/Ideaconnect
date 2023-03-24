caecb36bd14a9cfefdfc2157f6bf14911650e487
StarJune <partis98@kakao.com>
2023-03-24 18:05:39 +0900

    CORS 일부 설정 (Method)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   const config = new DocumentBuilder()
     .setTitle('Idea Connect API Document')
     .setDescription(
