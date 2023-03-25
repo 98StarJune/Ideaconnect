3c23cbf0740b8e4f10fdfc272f8a41b3f149f9bd
StarJune <partis98@kakao.com>
2023-03-25 14:20:50 +0900

    httpchat.entity 불필요한 내용 삭제 (DataResponseDto 이용으로 합병)

--- src/model/dto/response/chat.create.response.dto.ts
+++ src/model/dto/response/chat.create.response.dto.ts
@@ -2,7 +2,6 @@ import { PickType } from '@nestjs/swagger';
 import { HttpchatEntity } from '../../httpchat.entity';
 
 export class chatCreateResponseDto extends PickType(HttpchatEntity, [
-  'nickname',
   'roomname',
   'statusCode',
 ]) {}
