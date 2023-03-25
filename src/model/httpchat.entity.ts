edd730def33b0c4717eeb58d4616de9f9accc575
StarJune <partis98@kakao.com>
2023-03-25 14:18:31 +0900

    httpchat.entity 불필요한 내용 삭제 (DataResponseDto 이용으로 합병)

--- src/model/httpchat.entity.ts
+++ src/model/httpchat.entity.ts
@@ -1,11 +1,5 @@
 export class HttpchatEntity {
   jwtid: string;
-  nickname: string;
   roomname: string;
   statusCode: number;
-  /**채팅 관련*/
-  _id: string; //나의 유저 ID
-  message: string;
-  sender: string;
-  date: string;
 }
