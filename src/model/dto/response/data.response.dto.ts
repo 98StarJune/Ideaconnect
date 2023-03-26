8ebb806a2900381973372b77a6e832bf7b6fd716
StarJune <partis98@kakao.com>
2023-03-26 13:14:49 +0900

    Data Response DTO data 세분화

--- src/model/dto/response/data.response.dto.ts
+++ src/model/dto/response/data.response.dto.ts
@@ -4,6 +4,8 @@ import { ResponseEntity } from '../../response.entity';
 export class DataResponseDto extends PickType(ResponseEntity, ['statusCode']) {
   @ApiProperty({ description: '데이터 결과값' })
   data: object;
+  chat_data: object;
+  room_data: object;
   nickname: string;
   _id: string;
 }
